e90099976edfa006337dc1457e7d220b3c407087
eriksmit <125769585+e-smit@users.noreply.github.com>
2023-10-31 20:03:56 +0100

    added code for initalising display

--- src/game.c
+++
@@ -1,2 +0,0 @@
-#include <IOShieldTemp.h>
-#include <IOShieldOled.h>
---
+++ src/t_rex.h
@@ -0,0 +1,6 @@
+/* THIS IS THE HEADER FILE FOR THE PROJECT*/
+
+void display_init(void);
+void display_update(void);
+void display_start(void);
+uint8_t spi_send_recv(uint8_t data);
